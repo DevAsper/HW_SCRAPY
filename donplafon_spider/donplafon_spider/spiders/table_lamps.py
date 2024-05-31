4ec51c79597ceca1fa06014ca5cb12e6822d5c47
DevAsper <asponomaryov@yandex.ru>
2024-05-31 13:46:19 +0300

    sum

--- donplafon_spider/donplafon_spider/spiders/table_lamps.py
+++ donplafon_spider/donplafon_spider/spiders/table_lamps.py
@@ -3,14 +3,14 @@ import scrapy
 class TableLampsSpider(scrapy.Spider):
     name = 'table_lamps'
     allowed_domains = ['donplafon.ru']
-    start_urls = ['https://donplafon.ru/catalog/nastolnye-lampy/']
+    start_urls = ['https://donplafon.ru/catalog/nastolnye/']
 
     def parse(self, response):
-        for product in response.css('div.product-item'):
+        for product in response.css('div.itemtype="https://schema.org/Product"'):
             yield {
-                'title': product.css('a.product-title::text').get().strip(),
-                'price': product.css('span.price::text').get().strip(),
-                'link': response.urljoin(product.css('a.product-title::attr(href)').get())
+                'title': product.css('class="productItem__title"').get().strip(),
+                'price': product.css('class="productItem__cost"').get().strip(),
+                'link': response.urljoin(product.css('class="productItem__href"').get())
             }
 
         next_page = response.css('a.page-next::attr(href)').get()
