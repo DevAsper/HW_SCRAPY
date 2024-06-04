99419da0f70bfa5152a7bdc84bf02bb63720e300
DevAsper <asponomaryov@yandex.ru>
2024-06-04 14:40:06 +0300

    update1

--- donplafon_spider/donplafon_spider/spiders/table_lamps.py
+++ donplafon_spider/donplafon_spider/spiders/table_lamps.py
@@ -1,18 +1,37 @@
 import scrapy
 
+
 class TableLampsSpider(scrapy.Spider):
     name = 'table_lamps'
-    allowed_domains = ['https://donplafon.ru']
-    start_urls = ['https://donplafon.ru/catalog/nastolnye']
+    allowed_domains = ['donplafon.ru']
+    start_urls = ['https://donplafon.ru/catalog/nastolnye/']
 
     def parse(self, response):
-        for product in response.css('div.productItem__container'):
-            yield {
-                'title': product.css('div.productItem__title').get().strip(),
-                'price': product.css('div.productItem__cost').get().strip(),
-                'link': response.urljoin(product.css('div.productItem__href').get())
-            }
+        for product in response.css('div.id="product-list"'):
+
+            title = product.css('div.data-name::text').get()
+            if title:
+                title = title.strip()
+
+            price = product.css('div.data-price::text').get()
+            if price:
+                price = price.strip()
+
+            link_element = product.css('a.itemprop="url"::attr(href)').get()
+            if link_element:
+                link = response.urljoin(link_element.strip())
+            else:
+                link = None
+
+
+            if title and price and link:
+                yield {
+                    'title': title,
+                    'price': price,
+                    'link': link
+                }
+
 
         next_page = response.css('a.page-next::attr(href)').get()
-        if next_page is not None:
+        if next_page:
             yield response.follow(next_page, self.parse)
